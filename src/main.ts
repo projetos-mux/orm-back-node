07181f5aca5646475d61ea2e9533c279da78f12e
Paulo Henrique Paiva <paulohenriquepaiva@ymail.com>
2026-04-24 20:30:43 -0300

    Alterações de funcionamento e novas features

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,14 @@ async function bootstrap() {
     })
   )
 
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+    ],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
